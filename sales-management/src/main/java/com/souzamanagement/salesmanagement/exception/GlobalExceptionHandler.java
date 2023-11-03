5e2f765c365d0481b51fcd8172e28dfa183764bd
Eduarda Menegueli de Souza <eduardasouza111213@gmail.com>
2023-11-03 18:32:41 -0300

    Already exists & Not found exception created

--- sales-management/src/main/java/com/souzamanagement/salesmanagement/exception/GlobalExceptionHandler.java
+++ sales-management/src/main/java/com/souzamanagement/salesmanagement/exception/GlobalExceptionHandler.java
@@ -1,36 +1,51 @@
 package com.souzamanagement.salesmanagement.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+
+
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<ExceptionResponse> handleAlreadyExistsException(AlreadyExistsException exception) {
         var exceptionResponse = new ExceptionResponse(exception.getMessage());
-        return new ResponseEntity<>("exceptionResponse", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ExceptionResponse> handleNotFoundException(NotFoundException exception) {
+        var exceptionResponse = new ExceptionResponse(exception.getMessage());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        var exceptionResponse = new ExceptionResponse(exception.getFieldError().getDefaultMessage());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ExceptionResponse> handleRuntimeException(RuntimeException exception) {
-        System.out.println("kkkkkkkkkkkkkkkkkk");
         var exceptionResponse = new ExceptionResponse(exception.getMessage());
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception exception) {
-        System.out.println("kkkkkkkkkkkkkkkkkk");
+    public ResponseEntity<ExceptionResponse> handleException(Exception exception) {
         var exceptionResponse = new ExceptionResponse(exception.getMessage());
-        return new ResponseEntity<>("exceptionResponse", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
